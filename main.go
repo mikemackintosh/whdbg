4413f6af88ca0d69fd6c9c2aec63048c940e5d83
Mike Mackintosh <m@zyp.io>
2016-09-28 15:03:52 -0700

    add some fancy output

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"net/http"
 	"net/http/httputil"
+	"strings"
 )
 
 // listenPort is supplied to ListenAndServer
@@ -19,6 +20,9 @@ func init() {
 
 // handler will write the request dump to the response and stdout
 func handler(w http.ResponseWriter, r *http.Request) {
+	// Output message
+	fmt.Printf("\033[38;5;45mNew Request Received:\033[0m\n")
+
 	// generate the request dump
 	requestDump, err := httputil.DumpRequest(r, true)
 	if err != nil {
@@ -38,6 +42,11 @@ func main() {
 
 	// Set the handler to output the request
 	http.HandleFunc("/", handler)
+
+	// Otuput that we're listening
+	fmt.Printf("\033[38;5;154mWebHook Debugger\033[0m by @mikemackintosh\n")
+	fmt.Printf("Listening on port: %s\n", listenPort)
+	fmt.Printf("%s\n\n", strings.Repeat("=", 24))
 
 	// Start the server
 	http.ListenAndServe(":"+listenPort, nil)
