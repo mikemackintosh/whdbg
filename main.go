3d4a2006a1dff2195535d2ed94790a5a0f7be8d6
Mike Mackintosh <m@zyp.io>
2016-09-28 09:45:19 -0700

    add listening port flag

--- main.go
+++ main.go
@@ -1,20 +1,44 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"net/http"
 	"net/http/httputil"
 )
 
+// listenPort is supplied to ListenAndServer
+var listenPort string
+
+func init() {
+	// Port to listen via HTTP on
+	//
+	// #-> default is 8080
+	flag.StringVar(&listenPort, "p", "8080", "Default listener port")
+}
+
+// handler will write the request dump to the response and stdout
 func handler(w http.ResponseWriter, r *http.Request) {
+	// generate the request dump
 	requestDump, err := httputil.DumpRequest(r, true)
 	if err != nil {
-		fmt.Println(err)
+		requestDump = []byte(err.Error())
 	}
-	fmt.Println(string(requestDump))
+
+	// Output locally
+	fmt.Printf(string(requestDump))
+
+	// Output to response
+	w.Write(requestDump)
 }
 
 func main() {
+	// Parse flags
+	flag.Parse()
+
+	// Set the handler to output the request
 	http.HandleFunc("/", handler)
-	http.ListenAndServe(":8080", nil)
+
+	// Start the server
+	http.ListenAndServe(":"+listenPort, nil)
 }
